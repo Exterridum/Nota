5cf8b4efe5dfca8cbe2ce483a2b8cd8b77303b38
matejkolimar <kolimar@inloop.eu>
2018-07-23 10:15:43 +0200

    Todo list completed with data source and delegate methods.

---
+++ Nota/Controller/TodoListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Nota
+//
+//  Created by Matej Kolimar on 17/07/2018.
+//  Copyright © 2018 Matej Kolimar. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Malta", "Mexico", "Miami"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .checkmark {
+                cell.accessoryType = .none
+            } else {
+                cell.accessoryType = .checkmark
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
---
+++ Nota/View/Custom Cell/TodoListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoListTableViewCell.swift
+//  Nota
+//
+//  Created by Matej Kolimar on 19/07/2018.
+//  Copyright © 2018 Matej Kolimar. All rights reserved.
+//
+
+import UIKit
+
+class TodoListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
