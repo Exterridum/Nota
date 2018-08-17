15eee8ab6bfdcec724adb7be0e5f2f19c96c27c7
matejkolimar <kolimar@inloop.eu>
2018-08-17 08:26:15 +0200

    SwipeTableViewController superclass added, CategoryViewController inherited and changed to run with this superclass

--- Nota/Controller/CategoryViewController.swift
+++ Nota/Controller/CategoryViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 import RealmSwift
 
-class CategoryViewController: UITableViewController {
-
+class CategoryViewController: SwipeTableViewController {
+    
     let realm = try! Realm()
     var categories: Results<Category>?
 
@@ -18,6 +18,7 @@ class CategoryViewController: UITableViewController {
         super.viewDidLoad()
         //TODO: - Load items from array
         loadCategories()
+        tableView.rowHeight = 80.0
     }
     
     //MARK: - Tableview Datasource Methods
@@ -26,7 +27,8 @@ class CategoryViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories added yet."
         return cell
     }
@@ -86,4 +88,19 @@ class CategoryViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    //MARK: - Delete Data From Swipe
+    override func updateModel(at indexPath: IndexPath) {
+        if let category = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(category)
+                }
+            } catch {
+                print("Error deleting category, \(error)")
+            }
+        }
+    }
+    
 }
+
+
